10ee6d3acb9d35d989a324b9e30820c94e254c78
unknown <you@example.com>
2021-12-14 22:41:51 +0100

    auth

---
+++ app/Http/Livewire/InstructorCursos.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class InstructorCursos extends Component
+{
+    public function render()
+    {
+        return view('livewire.instructor-cursos');
+    }
+}
